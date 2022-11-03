a84a96fdb551d8600d46e85051edc3f42053ecc9
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-11-03 10:50:38 -0500

    arithmetic operations working

--- src/ParsePython.hs
+++ src/ParsePython.hs
@@ -6,39 +6,86 @@ import Data.Void
 
 type Parser = Parsec Void String
 
-data Python = Node Content [Python] deriving (Show, Eq)
+-- data Python = Node Content [Python] deriving (Show, Eq)
 
-data Content = Start | Arithmetic String | Assignment String | Number String deriving (Show, Eq)
+data Content = Start [Content] | Arith Char [Content] | Num String | Assignment String [Content] deriving (Show, Eq)
 
-parsePython :: String -> Either String Python
+parsePython :: String -> Either String Content
 parsePython input = case parse pythonFile "" input of
     Left bundle ->  Left $ errorBundlePretty bundle
     Right result -> Right result
 
-pythonFile :: Parser Python
+pythonFile :: Parser Content
 pythonFile = do
-    s <- many statement
-    return $ Node Start s
+    s <- statement
+    return $ Start [s]
 
-statement :: Parser Python
+statement :: Parser Content
 statement = do
     s <- arithmetic
     return s 
 
-arithmetic :: Parser Python
-arithmetic = do
-    n1 <- number <* hspace
+arithmetic :: Parser Content
+arithmetic = (try (do 
+    a1 <- ((char '(' *> arithmetic <* char ')') <|> number) <* hspace
     op <- arithOperator <* hspace
-    n2 <- number <* hspace
-    return $ op [n1, n2]
+    a2 <- arithmetic
+    return $ Arith op [a1, a2]))
+    <|> (char '(' *> arithmetic <* char ')')
+    <|> number
 
-number :: Parser Python
+number :: Parser Content
 number = do
+    sign <- string "-" <|> string ""
     start <- some digitChar
-    return $ Node (Number start) []
+    end <- char '.' *> some digitChar <|> string ""
+    return $ Num $ sign++start++(if null end then "" else '.':end)
 
-arithOperator :: Parser Python
-arithOperator = do
-    op <- oneOf "+-/*%"
-    return $ Node (Arithmetic [op]) []
+arithOperator :: Parser Char
+arithOperator = oneOf "+-/*%"
+
+
+-- arithmetic :: Parser Python
+-- arithmetic = (char '(' *> arithmetic <* char ')') 
+--     <|> try (do
+--     a1 <- number <* hspace
+--     op <- arithOperator <* hspace
+--     a2 <- arithmetic
+--     return $ Node (Arithmetic op) [a1, a2])
+--     <|> try (do
+--     a1 <- arithmetic <* hspace
+--     op <- arithOperator <* hspace
+--     a2 <- number
+--     return $ Node (Arithmetic op) [a1, a2])
+--     -- <|> try (do
+--     -- a1 <- arithmetic <* hspace
+--     -- op <- arithOperator <* hspace
+--     -- a2 <- arithmetic
+--     -- return $ Node (Arithmetic op) [a1, a2])
+--     <|> number
+
+-- arithmetic :: Parser Content
+-- arithmetic = do
+--     first <- term
+
+-- term :: Parser Content
+-- term = (char '(' *> arithmetic <* char ')')
+--     <|> (do
+--             n <- number
+--             op <- arithOperator
+--             a <- arithmetic
+--             return $ Node (Arithmetic op) [n, a])
+
+
+
+-- expr, term :: Parser AExp
+-- expr = do
+--     n <- term
+--     rest <- optional $ tok "*" *> expr
+--     return $ maybe n (Mult n) rest
+-- term = N <$> numParser
+--     <|> V <$> strParser
+--     <|> parenthesised expr
+
+-- parenthesised = between (char '(') (char ')')
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,10 +4,28 @@ import Test.Hspec
 main :: IO ()
 main = hspec $ do
     describe "Python parser tests" $ do
-        it "empty input" $ do
-            parsePython "" `shouldBe` (Right $ Node Start []) 
+        -- it "empty input" $ do
+        --     parsePython "" `shouldBe` (Right $ Start []) 
         
         describe "arithmetic" $ do
-            it "simple addition" $ do
-                parsePython "18 + 987" `shouldBe` (Right $ Node Start [Node (Arithmetic "+") [Node (Number "18") [], Node (Number "987") []]])
+            it "simple operation" $ do
+                parsePython "18 + 987" `shouldBe` (Right $ Start [Arith '+' [Num "18", Num "987"]])
+
+            it "three element operation" $ do
+                parsePython "10 + 20 * 30" `shouldBe` (Right $ Start [Arith '+' [Num "10", Arith '*' [Num "20", Num "30"]]])
+
+            it "four element operation" $ do
+                parsePython "10 + 20 * 30 / 100" `shouldBe` (Right $ Start [Arith '+' [Num "10", Arith '*' [Num "20", Arith '/' [Num "30", Num "100"]]]])
+
+            it "left parens" $ do
+                parsePython "(1 + 2) + 3" `shouldBe` (Right $ Start [Arith '+' [Arith '+' [Num "1", Num "2"], Num "3"]])
+
+            it "right parens" $ do
+                parsePython "10 + (20 * 30)" `shouldBe` (Right $ Start [Arith '+' [Num "10", Arith '*' [Num "20", Num "30"]]])
+
+            it "pointless parens" $ do 
+                parsePython "(10 % 3)" `shouldBe` (Right $ Start [Arith '%' [Num "10", Num "3"]])
+
+            it "sets of parens" $ do
+                parsePython "((98.7 * -90) + (0.00009 - -89))" `shouldBe` (Right $ Start [Arith '+' [Arith '*' [Num "98.7", Num "-90"], Arith '-' [Num "0.00009", Num "-89"]]])
 
